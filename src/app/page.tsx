6673328df8f573ee2ee1f48b4e55ea262f53e9a4
ginosyan00 <gurgenginosyan@neetrino.com>
2026-03-20 16:06:21 +0400

    0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import { LandingNav } from "@/components/landing/LandingNav";
-import { LandingSections } from "@/components/landing/LandingSections";
-
 export default function Home() {
-  return (
-    <>
-      <LandingNav />
-      <main className="min-h-screen">
-        <LandingSections />
-      </main>
-    </>
-  );
+  return <main className="min-h-screen bg-white" />;
 }
