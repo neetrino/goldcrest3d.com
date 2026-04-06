2f20a4b94d1df7c97aa41a91c445ab7c961b4af7
ginosyan00 <gurgenginosyan@neetrino.com>
2026-04-06 17:53:09 +0400

    bug fix in scroollll

--- src/app/admin/leads/page.tsx
+++ src/app/admin/leads/page.tsx
@@ -53,7 +53,7 @@ export default async function AdminLeadsPage({
   }
 
   return (
-    <div className="flex h-full min-h-0 flex-1 flex-col">
+    <div className="flex h-full min-h-0 flex-1 flex-col overflow-hidden">
       <AdminLeadsInbox leads={listItems} selectedLead={selectedLead} />
     </div>
   );
